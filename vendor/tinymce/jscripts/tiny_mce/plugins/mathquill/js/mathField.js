51bc153bd55d580502a2acde62d1c6634f155532
Ian Davis <jungziege@gmail.com>
2012-09-30 23:50:51 -0700

    Added dynamic resizing to the mathquill popup
    Specified a know margin on the mathField.html so that we always know
    exactly how much padding outside of the mathquill-editable there is.

--- vendor/tinymce/jscripts/tiny_mce/plugins/mathquill/js/mathField.js
+++ vendor/tinymce/jscripts/tiny_mce/plugins/mathquill/js/mathField.js
@@ -3,14 +3,47 @@ tinyMCEPopup.requireLangPack();
 var MathquillDialog = {
   init : function() {
     var $ = window.top.$;
-    this.mathquill = $('.mathquill-editor', document).mathquill('editor')
+    this.mathquill  = $('.mathquill-editor', document).mathquill('editor')
       .keydown(function(e) {
         if (e.which === 13) {
           tinyMCEPopup.close();
         }
       });
+
+    this.mathquill.on('keydown keypress', function() {
+      setTimeout(function() {
+        MathquillDialog.fitWindow();
+      });
+    });
+
     var existing = tinyMCEPopup.getWindowArg('existing_latex');
     this.mathquill.mathquill('latex', existing).mathquill('redraw').focus();
+
+    this.fitWindow();
+  },
+
+  fitWindow : function() { // someday move to init code so global in tinyMCEPopup
+    win = tinyMCEPopup.id;
+    var dw = dh = 0;
+    var w = this.mathquill.outerWidth()+10;
+    var h = this.mathquill.outerHeight()+10;
+
+    // Now trick window into thinking it's that size.
+    // so all internal resizing gets done properly..
+    tinyMCEPopup.params.mce_height = h;
+    tinyMCEPopup.params.mce_width = w;
+    tinyMCEPopup.resizeToInnerSize();
+
+    // Now move picture back to center.
+    var vpw = tinyMCE.DOM.getViewPort().w;
+    var vph = tinyMCE.DOM.getViewPort().h;
+    var top = vph > h ? (vph-h)/2 : 0;
+    var left = vpw > w ? (vpw-w)/2 : 0;
+
+    // recenter.
+    var popup = tinyMCE.DOM.doc.getElementById(tinyMCEPopup.id);
+    popup.style.top = top+"px";
+    popup.style.left = left+"px";
   },
 
   getLatex : function() {
