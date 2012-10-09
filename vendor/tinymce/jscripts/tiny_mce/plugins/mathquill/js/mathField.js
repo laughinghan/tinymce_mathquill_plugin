e0f919ac0f04a6b441998af91a797f4642ecbc41
Han Wei <laughinghan@gmail.com>
2012-10-09 03:21:07 -0700

    move fitWindow() into MathquillDialog.init
    use closure to simplify code a wee bit

--- vendor/tinymce/jscripts/tiny_mce/plugins/mathquill/js/mathField.js
+++ vendor/tinymce/jscripts/tiny_mce/plugins/mathquill/js/mathField.js
@@ -10,40 +10,36 @@ var MathquillDialog = {
         }
       });
 
-    mathquill.on('keydown keypress', function() {
-      setTimeout(function() {
-        MathquillDialog.fitWindow();
-      });
-    });
+    mathquill.on('keydown keypress', function() { setTimeout(fitWindow); });
 
     var existing = tinyMCEPopup.getWindowArg('existing_latex');
     mathquill.mathquill('latex', existing).mathquill('redraw').focus();
 
-    this.fitWindow();
-  },
+    fitWindow();
 
-  fitWindow : function() { // someday move to init code so global in tinyMCEPopup
-    win = tinyMCEPopup.id;
-    var dw = dh = 0;
-    var w = this.mathquill.outerWidth()+10;
-    var h = this.mathquill.outerHeight()+10;
+    function fitWindow() {
+      win = tinyMCEPopup.id;
+      var dw = dh = 0;
+      var w = mathquill.outerWidth()+10;
+      var h = mathquill.outerHeight()+10;
 
-    // Now trick window into thinking it's that size.
-    // so all internal resizing gets done properly..
-    tinyMCEPopup.params.mce_height = h;
-    tinyMCEPopup.params.mce_width = w;
-    tinyMCEPopup.resizeToInnerSize();
+      // Now trick window into thinking it's that size.
+      // so all internal resizing gets done properly..
+      tinyMCEPopup.params.mce_height = h;
+      tinyMCEPopup.params.mce_width = w;
+      tinyMCEPopup.resizeToInnerSize();
 
-    // Now move picture back to center.
-    var vpw = tinyMCE.DOM.getViewPort().w;
-    var vph = tinyMCE.DOM.getViewPort().h;
-    var top = vph > h ? (vph-h)/2 : 0;
-    var left = vpw > w ? (vpw-w)/2 : 0;
+      // Now move picture back to center.
+      var vpw = tinyMCE.DOM.getViewPort().w;
+      var vph = tinyMCE.DOM.getViewPort().h;
+      var top = vph > h ? (vph-h)/2 : 0;
+      var left = vpw > w ? (vpw-w)/2 : 0;
 
-    // recenter.
-    var popup = tinyMCE.DOM.doc.getElementById(tinyMCEPopup.id);
-    popup.style.top = top+"px";
-    popup.style.left = left+"px";
+      // recenter.
+      var popup = tinyMCE.DOM.doc.getElementById(tinyMCEPopup.id);
+      popup.style.top = top+"px";
+      popup.style.left = left+"px";
+    }
   },
 
   getLatex : function() {
